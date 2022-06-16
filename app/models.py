f4ad7d037dfea89bcd4ac77c85bdf847a2becfab
Grace Nyutu <graceynyutu@gmail.com>
2022-06-16 11:44:37 +0300

    create neighborhood class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Neighborhood(models.Model):
+    hoodname = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    occupants = models.IntegerField()
+    admin = models.ForeignKey()

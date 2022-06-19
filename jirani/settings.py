aea589b7c3b63abb8825b99de0af8bbff090b99f
Grace Nyutu <graceynyutu@gmail.com>
2022-06-19 20:41:55 +0300

    .

--- jirani/settings.py
+++ jirani/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from app.apps import AppConfig
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
